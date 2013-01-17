ef34bc1e30ee83a7440ce94d9990100811886735
chicco.colantoni <chicco.colantoni@f2c33e9a-98a0-4916-0173-56d017a38c9b>
2013-01-17 13:16:08 +0000

    Adesso il passaggio da un'activity ad un'altra funziona correttamente. Aggiunto il passaggio tra MainActivity e IndexActivity dell'informazione sull'ID dell'user.

--- Interventix/src/com/federicocolantoni/projects/interventix/IndexActivity.java
+++ Interventix/src/com/federicocolantoni/projects/interventix/IndexActivity.java
@@ -3,17 +3,29 @@ package com.federicocolantoni.projects.interventix;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.Window;
 
 import com.federicocolanoni.projects.interventix.R;
 
 public class IndexActivity extends Activity {
 
+    private String idUser;
+    private final String debugTag = "xxx";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
 	super.onCreate(savedInstanceState);
+	requestWindowFeature(Window.FEATURE_NO_TITLE);
+
 	setContentView(R.layout.activity_index);
+
+	Bundle extra = getIntent().getExtras();
+	setIdUser(extra.getString("iduser"));
+
+	Log.d(debugTag, "ID user: " + getIdUser());
     }
 
     @Override
@@ -22,5 +34,22 @@ public class IndexActivity extends Activity {
 	// Inflate the menu; this adds items to the action bar if it is present.
 	getMenuInflater().inflate(R.menu.activity_index, menu);
 	return true;
+    }
+
+    /**
+     * @return the idUser
+     */
+    public String getIdUser() {
+
+	return idUser;
+    }
+
+    /**
+     * @param idUser
+     *            the idUser to set
+     */
+    public void setIdUser(String idUser) {
+
+	this.idUser = idUser;
     }
 }
