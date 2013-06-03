9e4324a13fe88ebd9b8aa4184f325d637d7ab063
chicco.colantoni <chicco.colantoni@f2c33e9a-98a0-4916-0173-56d017a38c9b>
2013-06-03 19:56:05 +0000

    Completata la visualizzazione delle informazioni dell'intervento nella lista degli interventi.

--- trunk/Interventix/src/com/federicocolantoni/projects/interventix/core/InterventionsAdapter.java
+++ trunk/Interventix/src/com/federicocolantoni/projects/interventix/core/InterventionsAdapter.java
@@ -1,9 +1,16 @@
 
 package com.federicocolantoni.projects.interventix.core;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.ExecutionException;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,17 +18,21 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.federicocolantoni.projects.interventix.R;
+import com.federicocolantoni.projects.interventix.data.DBContract.ClienteDB;
+import com.federicocolantoni.projects.interventix.intervento.Cliente;
 import com.federicocolantoni.projects.interventix.intervento.Intervento;
 
 public class InterventionsAdapter extends BaseAdapter {
 
     private LayoutInflater inflater;
     private List<Intervento> list;
+    private Context context;
 
     public InterventionsAdapter(Context context, List<Intervento> list) {
 
 	this.list = list;
 	inflater = LayoutInflater.from(context);
+	this.context = context;
     }
 
     @Override
@@ -55,6 +66,62 @@ public class InterventionsAdapter extends BaseAdapter {
 		.findViewById(R.id.tv_nome);
 	nomeIntervento.setText("Intervento n° " + interv.getmIdIntervento());
 
+	TextView clienteIntervento = (TextView) convertView
+		.findViewById(R.id.tv_cliente);
+
+	GetCliente cliente = new GetCliente(context);
+	cliente.execute(interv.getmIdCliente());
+
+	try {
+	    clienteIntervento.setText(cliente.get().getmNominativo() + " - ");
+	} catch (InterruptedException e) {
+	    e.printStackTrace();
+	} catch (ExecutionException e) {
+	    e.printStackTrace();
+	}
+
+	TextView dataIntervento = (TextView) convertView
+		.findViewById(R.id.tv_data);
+	dataIntervento.setText(new SimpleDateFormat("dd/MM/yyyy", Locale.ITALY)
+		.format(new Date(interv.getmDataOra())));
+
 	return convertView;
+    }
+
+    private class GetCliente extends AsyncTask<Long, Void, Cliente> {
+
+	private Context context;
+
+	public GetCliente(Context context) {
+
+	    this.context = context.getApplicationContext();
+	}
+
+	@Override
+	protected Cliente doInBackground(Long... params) {
+
+	    ContentResolver cr = context.getContentResolver();
+
+	    String selection = ClienteDB.Fields.TYPE + "='"
+		    + ClienteDB.CLIENTE_ITEM_TYPE + "' AND "
+		    + ClienteDB.Fields.ID_CLIENTE + "=" + params[0];
+
+	    Cursor cursor = cr.query(ClienteDB.CONTENT_URI, null, selection,
+		    null, null);
+
+	    Cliente cliente = new Cliente();
+
+	    if (cursor.moveToFirst()) {
+		cliente.setmNominativo(cursor.getString(cursor
+			.getColumnIndex(ClienteDB.Fields.NOMINATIVO)));
+	    }
+
+	    return cliente;
+	}
+
+	@Override
+	protected void onPostExecute(Cliente result) {
+
+	}
     }
 }
