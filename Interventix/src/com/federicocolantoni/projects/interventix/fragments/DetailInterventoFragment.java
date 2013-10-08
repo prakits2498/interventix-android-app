f0a72c9b7ea916a685f5285b2ace14d4cf33448b
chicco.colantoni <chicco.colantoni@f2c33e9a-98a0-4916-0173-56d017a38c9b>
2013-10-08 19:33:24 +0000

    Aggiunta dei tecnici a un dettaglio.

--- Interventix/src/com/federicocolantoni/projects/interventix/fragments/DetailInterventoFragment.java
+++ Interventix/src/com/federicocolantoni/projects/interventix/fragments/DetailInterventoFragment.java
@@ -67,7 +67,7 @@ public class DetailInterventoFragment extends SherlockFragment {
 	
 	return setupViewsDetailIntervernto(view);
     }
-
+    
     private View setupViewsDetailIntervernto(final View view) {
 	if (sId_Dettaglio_Intervento != -1l) {
 	    DettaglioIntervento dettInterv = null;
@@ -120,6 +120,14 @@ public class DetailInterventoFragment extends SherlockFragment {
 	    
 	    TextView tv_row_tecnici_dett = (TextView) row_tecnici_dett.findViewById(R.id.tv_row_tecnici_dettaglio);
 	    tv_row_tecnici_dett.setText("" + dettInterv.getmTecnici().length());
+	    
+	    row_tecnici_dett.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+		    
+		}
+	    });
 	    
 	    View row_descrizione_dett = view.findViewById(R.id.row_descrizione_dettaglio);
 	    
@@ -415,10 +423,354 @@ public class DetailInterventoFragment extends SherlockFragment {
 	}
 	else {
 	    
-	    InterventixToast.makeToast(getSherlockActivity(), "Creazione nuovo dettaglio per l'intervento", Toast.LENGTH_SHORT);
+	    // InterventixToast.makeToast(getSherlockActivity(),
+	    // "Creazione nuovo dettaglio per l'intervento",
+	    // Toast.LENGTH_SHORT);
+	    
+	    addNewDetail(view);
 	}
 	
 	return view;
+    }
+    
+    private void addNewDetail(final View view) {
+	
+	TextView tv_dett_interv = (TextView) view.findViewById(R.id.tv_dett_interv);
+	
+	tv_dett_interv.setText("Dettaglio " + sId_Dettaglio_Intervento);
+	
+	View row_tipo_dett = view.findViewById(R.id.row_tipo_dettaglio);
+	
+	row_tipo_dett.setOnClickListener(new OnClickListener() {
+	    
+	    @Override
+	    public void onClick(View v) {
+		
+		new SetTipo().show(getFragmentManager(), Constants.TIPO_DETTAGLIO_DIALOG_FRAGMENT);
+	    }
+	});
+	
+	TextView tv_row_tipo_dett = (TextView) row_tipo_dett.findViewById(R.id.tv_row_tipo_dettaglio);
+	tv_row_tipo_dett.setText("");
+	
+	View row_oggetto_dett = view.findViewById(R.id.row_oggetto_dettaglio);
+	
+	row_oggetto_dett.setOnClickListener(new OnClickListener() {
+	    
+	    @Override
+	    public void onClick(View v) {
+		
+		new SetOggetto().show(getFragmentManager(), Constants.OGGETTO_DETTAGLIO_DIALOG_FRAGMENT);
+	    }
+	});
+	
+	TextView tv_row_oggetto_dett = (TextView) row_oggetto_dett.findViewById(R.id.tv_row_oggetto_dettaglio);
+	tv_row_oggetto_dett.setText("");
+	
+	View row_tecnici_dett = view.findViewById(R.id.row_tecnici_dettaglio);
+	
+	TextView tv_row_tecnici_dett = (TextView) row_tecnici_dett.findViewById(R.id.tv_row_tecnici_dettaglio);
+	tv_row_tecnici_dett.setText("");
+	
+	row_tecnici_dett.setOnClickListener(new OnClickListener() {
+	    
+	    @Override
+	    public void onClick(View v) {
+		
+	    }
+	});
+	
+	View row_descrizione_dett = view.findViewById(R.id.row_descrizione_dettaglio);
+	
+	row_descrizione_dett.setOnClickListener(new OnClickListener() {
+	    
+	    @Override
+	    public void onClick(View v) {
+		
+		new SetDecrizione().show(getFragmentManager(), Constants.DESCRIZIONE_DETTAGLIO_DIALOG_FRAGMENT);
+	    }
+	});
+	
+	TextView tv_row_descrizione_dett = (TextView) row_descrizione_dett.findViewById(R.id.tv_row_descrizione_dettaglio);
+	tv_row_descrizione_dett.setText("");
+	
+	final View row_inizio_dett = view.findViewById(R.id.row_inizio_dettaglio);
+	
+	row_inizio_dett.setOnClickListener(new OnClickListener() {
+	    
+	    @Override
+	    public void onClick(View v) {
+		
+		final TextView tv_row_inizio_dett = (TextView) row_inizio_dett.findViewById(R.id.tv_row_inizio_dettaglio);
+		
+		final Dialog dateTimeDialog = new Dialog(getSherlockActivity());
+		
+		final RelativeLayout dateTimeDialogView = (RelativeLayout) getSherlockActivity().getLayoutInflater().inflate(R.layout.date_time_dialog, null);
+		
+		final DateTimePicker dateTimePicker = (DateTimePicker) dateTimeDialogView.findViewById(R.id.DateTimePicker);
+		
+		DateTime dt = null;
+		
+		dt = DateTime.parse(tv_row_inizio_dett.getText().toString(), DateTimeFormat.forPattern("dd/MM/yyyy HH:mm"));
+		
+		dateTimePicker.setDateTime(dt);
+		
+		dateTimePicker.setDateChangedListener(new DateWatcher() {
+		    
+		    @Override
+		    public void onDateChanged(Calendar c) {
+			
+		    }
+		});
+		
+		((Button) dateTimeDialogView.findViewById(R.id.SetDateTime)).setOnClickListener(new OnClickListener() {
+		    
+		    @Override
+		    public void onClick(View v) {
+			
+			dateTimePicker.clearFocus();
+			
+			DateTime dt_inizio = new DateTime(dateTimePicker.getYear(), dateTimePicker.getMonth(), dateTimePicker.getDay(), dateTimePicker.getHour(), dateTimePicker.getMinute(), DateTimeZone.forID("Europe/Rome"));
+			
+			TextView tv_dett_ore_tot = (TextView) getSherlockActivity().findViewById(R.id.tv_row_tot_ore_dettaglio);
+			TextView tv_dett_fine = (TextView) getSherlockActivity().findViewById(R.id.tv_row_fine_dettaglio);
+			
+			DateTime dt_fine = null;
+			
+			DateTimeFormatter fmt = DateTimeFormat.forPattern("dd/MM/yyyy HH:mm");
+			fmt.withZone(DateTimeZone.forID("Europe/Rome"));
+			
+			dt_fine = fmt.parseDateTime(tv_dett_fine.getText().toString());
+			
+			if (dt_fine.toDate().getTime() < dt_inizio.toDate().getTime()) {
+			    InterventixToast.makeToast(getSherlockActivity(), "Errore! Inizio intervento maggiore di fine intervento", Toast.LENGTH_LONG);
+			}
+			else {
+			    
+			    tv_row_inizio_dett.setText(dt_inizio.toString("dd/MM/yyyy HH:mm"));
+			    
+			    DateTime dt_tot_ore = dt_fine.minus(dt_inizio.toDate().getTime());
+			    
+			    tv_dett_ore_tot.setText(dt_tot_ore.toString("HH:mm"));
+			    
+			    SaveChangesDettaglioInterventoAsyncQueryHandler saveChanges = new SaveChangesDettaglioInterventoAsyncQueryHandler(getSherlockActivity());
+			    
+			    ContentValues values = new ContentValues();
+			    values.put(DettaglioInterventoDB.Fields.INIZIO, dt_inizio.toDate().getTime());
+			    values.put(DettaglioInterventoDB.Fields.MODIFICATO, "M");
+			    
+			    String selection = DettaglioInterventoDB.Fields.TYPE + " = ? AND " + DettaglioInterventoDB.Fields.ID_DETTAGLIO_INTERVENTO + " = ?";
+			    
+			    String[] selectionArgs = new String[] {
+				    DettaglioInterventoDB.DETTAGLIO_INTERVENTO_ITEM_TYPE, "" + sId_Dettaglio_Intervento
+			    };
+			    
+			    saveChanges.startUpdate(Constants.TOKEN_ORA_INIZIO_DETTAGLIO, null, DettaglioInterventoDB.CONTENT_URI, values, selection, selectionArgs);
+			    
+			    SharedPreferences prefs = getSherlockActivity().getSharedPreferences(Constants.PREFERENCES, Context.MODE_PRIVATE);
+			    
+			    final Editor edit = prefs.edit();
+			    
+			    edit.putBoolean(Constants.DETT_INTERV_MODIFIED, true);
+			    
+			    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+				edit.apply();
+			    }
+			    else {
+				new Thread(new Runnable() {
+				    
+				    @Override
+				    public void run() {
+					edit.commit();
+				    }
+				}).start();
+			    }
+			}
+			
+			dateTimeDialog.dismiss();
+		    }
+		});
+		
+		((Button) dateTimeDialogView.findViewById(R.id.CancelDialog)).setOnClickListener(new View.OnClickListener() {
+		    
+		    @Override
+		    public void onClick(View v) {
+			
+			dateTimeDialog.cancel();
+		    }
+		});
+		
+		((Button) dateTimeDialogView.findViewById(R.id.ResetDateTime)).setOnClickListener(new View.OnClickListener() {
+		    
+		    @Override
+		    public void onClick(View v) {
+			
+			DateTime dt = null;
+			
+			DateTimeFormatter fmt = DateTimeFormat.forPattern("dd/MM/yyyy HH:mm");
+			fmt.withZone(DateTimeZone.forID("Europe/Rome"));
+			
+			dt = fmt.parseDateTime(tv_row_inizio_dett.getText().toString());
+			
+			dateTimePicker.setDateTime(dt);
+		    }
+		});
+		
+		dateTimeDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		dateTimeDialog.setContentView(dateTimeDialogView);
+		dateTimeDialog.setCancelable(false);
+		dateTimeDialog.show();
+	    }
+	});
+	
+	TextView tv_row_inizio_dett = (TextView) row_inizio_dett.findViewById(R.id.tv_row_inizio_dettaglio);
+	
+	DateTime dt_inizio = new DateTime(DateTime.now(), DateTimeZone.forID("Europe/Rome"));
+	
+	tv_row_inizio_dett.setText(dt_inizio.toString("dd/MM/yyyy HH:mm", Locale.ITALY));
+	
+	final View row_fine_dett = view.findViewById(R.id.row_fine_dettaglio);
+	
+	row_fine_dett.setOnClickListener(new OnClickListener() {
+	    
+	    @Override
+	    public void onClick(View v) {
+		
+		final TextView tv_row_fine_dett = (TextView) row_fine_dett.findViewById(R.id.tv_row_fine_dettaglio);
+		
+		final Dialog dateTimeDialog = new Dialog(getSherlockActivity());
+		
+		final RelativeLayout dateTimeDialogView = (RelativeLayout) getSherlockActivity().getLayoutInflater().inflate(R.layout.date_time_dialog, null);
+		
+		final DateTimePicker dateTimePicker = (DateTimePicker) dateTimeDialogView.findViewById(R.id.DateTimePicker);
+		
+		DateTime dt = null;
+		
+		dt = DateTime.parse(tv_row_fine_dett.getText().toString(), DateTimeFormat.forPattern("dd/MM/yyyy HH:mm"));
+		
+		dateTimePicker.setDateTime(dt);
+		
+		dateTimePicker.setDateChangedListener(new DateWatcher() {
+		    
+		    @Override
+		    public void onDateChanged(Calendar c) {
+			
+		    }
+		});
+		
+		((Button) dateTimeDialogView.findViewById(R.id.SetDateTime)).setOnClickListener(new OnClickListener() {
+		    
+		    @Override
+		    public void onClick(View v) {
+			
+			dateTimePicker.clearFocus();
+			
+			DateTime dt_fine = new DateTime(dateTimePicker.getYear(), dateTimePicker.getMonth(), dateTimePicker.getDay(), dateTimePicker.getHour(), dateTimePicker.getMinute(), DateTimeZone.forID("Europe/Rome"));
+			
+			TextView tv_dett_ore_tot = (TextView) getSherlockActivity().findViewById(R.id.tv_row_tot_ore_dettaglio);
+			TextView tv_dett_inizio = (TextView) getSherlockActivity().findViewById(R.id.tv_row_inizio_dettaglio);
+			
+			DateTime dt_inizio = null;
+			
+			DateTimeFormatter fmt = DateTimeFormat.forPattern("dd/MM/yyyy HH:mm");
+			fmt.withZone(DateTimeZone.forID("Europe/Rome"));
+			
+			dt_inizio = fmt.parseDateTime(tv_dett_inizio.getText().toString());
+			
+			if (dt_fine.toDate().getTime() < dt_inizio.toDate().getTime()) {
+			    InterventixToast.makeToast(getSherlockActivity(), "Errore! Inizio intervento maggiore di fine intervento", Toast.LENGTH_LONG);
+			}
+			else {
+			    
+			    tv_row_fine_dett.setText(dt_fine.toString("dd/MM/yyyy HH:mm"));
+			    
+			    DateTime dt_tot_ore = dt_fine.minus(dt_inizio.toDate().getTime());
+			    
+			    tv_dett_ore_tot.setText(dt_tot_ore.toString("HH:mm"));
+			    
+			    SaveChangesDettaglioInterventoAsyncQueryHandler saveChanges = new SaveChangesDettaglioInterventoAsyncQueryHandler(getSherlockActivity());
+			    
+			    ContentValues values = new ContentValues();
+			    values.put(DettaglioInterventoDB.Fields.FINE, dt_fine.toDate().getTime());
+			    values.put(DettaglioInterventoDB.Fields.MODIFICATO, "M");
+			    
+			    String selection = DettaglioInterventoDB.Fields.TYPE + " = ? AND " + DettaglioInterventoDB.Fields.ID_DETTAGLIO_INTERVENTO + " = ?";
+			    
+			    String[] selectionArgs = new String[] {
+				    DettaglioInterventoDB.DETTAGLIO_INTERVENTO_ITEM_TYPE, "" + sId_Dettaglio_Intervento
+			    };
+			    
+			    saveChanges.startUpdate(Constants.TOKEN_ORA_FINE_DETTAGLIO, null, DettaglioInterventoDB.CONTENT_URI, values, selection, selectionArgs);
+			    
+			    SharedPreferences prefs = getSherlockActivity().getSharedPreferences(Constants.PREFERENCES, Context.MODE_PRIVATE);
+			    
+			    final Editor edit = prefs.edit();
+			    
+			    edit.putBoolean(Constants.DETT_INTERV_MODIFIED, true);
+			    
+			    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+				edit.apply();
+			    }
+			    else {
+				new Thread(new Runnable() {
+				    
+				    @Override
+				    public void run() {
+					edit.commit();
+				    }
+				}).start();
+			    }
+			}
+			
+			dateTimeDialog.dismiss();
+		    }
+		});
+		
+		((Button) dateTimeDialogView.findViewById(R.id.CancelDialog)).setOnClickListener(new View.OnClickListener() {
+		    
+		    @Override
+		    public void onClick(View v) {
+			
+			dateTimeDialog.cancel();
+		    }
+		});
+		
+		((Button) dateTimeDialogView.findViewById(R.id.ResetDateTime)).setOnClickListener(new View.OnClickListener() {
+		    
+		    @Override
+		    public void onClick(View v) {
+			
+			DateTime dt = null;
+			
+			DateTimeFormatter fmt = DateTimeFormat.forPattern("dd/MM/yyyy HH:mm");
+			fmt.withZone(DateTimeZone.forID("Europe/Rome"));
+			
+			dt = fmt.parseDateTime(tv_row_fine_dett.getText().toString());
+			
+			dateTimePicker.setDateTime(dt);
+		    }
+		});
+		
+		dateTimeDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		dateTimeDialog.setContentView(dateTimeDialogView);
+		dateTimeDialog.setCancelable(false);
+		dateTimeDialog.show();
+	    }
+	});
+	
+	TextView tv_row_fine_dett = (TextView) row_fine_dett.findViewById(R.id.tv_row_fine_dettaglio);
+	
+	DateTime dt_fine = new DateTime(DateTime.now(), DateTimeZone.forID("Europe/Rome"));
+	
+	tv_row_fine_dett.setText(dt_fine.toString("dd/MM/yyyy HH:mm", Locale.ITALY));
+	
+	View row_tot_ore_dett = view.findViewById(R.id.row_tot_ore_dettaglio);
+	
+	TextView tv_row_tot_ore_dett = (TextView) row_tot_ore_dett.findViewById(R.id.tv_row_tot_ore_dettaglio);
+	
+	DateTime dt_tot_ore = new DateTime(dt_fine.toDate().getTime() - dt_inizio.toDate().getTime(), DateTimeZone.forID("Europe/Rome"));
+	
+	tv_row_tot_ore_dett.setText(dt_tot_ore.toString(DateTimeFormat.forPattern("HH:mm")));
     }
     
     public static class SetTipo extends SherlockDialogFragment implements DialogInterface.OnClickListener {
