5b26eb3161e5729a43d4882300e1467dd532cf88
chicco.colantoni <chicco.colantoni@f2c33e9a-98a0-4916-0173-56d017a38c9b>
2014-02-02 13:04:12 +0000

    Versione 0.0.600
    Refactoring dei nomi dei packages delle classi della UI.
    Iniziata l'implementazione della gestione dell'account.

---
+++ trunk/Interventix/src/com/federicocolantoni/projects/interventix/authentication/InterventixAuthenticator.java
@@ -0,0 +1,106 @@
+package com.federicocolantoni.projects.interventix.authentication;
+
+import static android.accounts.AccountManager.KEY_BOOLEAN_RESULT;
+import android.accounts.AbstractAccountAuthenticator;
+import android.accounts.Account;
+import android.accounts.AccountAuthenticatorResponse;
+import android.accounts.AccountManager;
+import android.accounts.NetworkErrorException;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+
+import com.federicocolantoni.projects.interventix.Constants;
+import com.federicocolantoni.projects.interventix.ui.activity.MainActivity_;
+
+public class InterventixAuthenticator extends AbstractAccountAuthenticator {
+
+	Context context;
+
+	public InterventixAuthenticator(Context context) {
+
+		super(context);
+
+		this.context = context;
+	}
+
+	@Override
+	public Bundle addAccount(AccountAuthenticatorResponse response, String accountType, String authTokenType, String[] requiredFeatures, Bundle options)
+			throws NetworkErrorException {
+
+		final Intent intent = new Intent(context, MainActivity_.class);
+		intent.putExtra(Constants.ACCOUNT_TYPE_INTERVENTIX, accountType);
+		intent.putExtra(Constants.ACCOUNT_AUTH_TOKEN, authTokenType);
+		intent.putExtra(Constants.ADDING_NEW_ACCOUNT, true);
+		intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response);
+
+		final Bundle bundle = new Bundle();
+		bundle.putParcelable(AccountManager.KEY_INTENT, intent);
+		return bundle;
+	}
+
+	@Override
+	public Bundle confirmCredentials(AccountAuthenticatorResponse arg0, Account arg1, Bundle arg2) throws NetworkErrorException {
+
+		return null;
+	}
+
+	@Override
+	public Bundle editProperties(AccountAuthenticatorResponse response, String accountType) {
+
+		return null;
+	}
+
+	@Override
+	public Bundle getAuthToken(AccountAuthenticatorResponse response, Account account, String authTokenType, Bundle options) throws NetworkErrorException {
+
+		if (!authTokenType.equals(Constants.ACCOUNT_AUTH_TOKEN)) {
+			final Bundle result = new Bundle();
+			result.putString(AccountManager.KEY_ERROR_MESSAGE, "invalid authTokenType");
+			return result;
+		}
+
+		final AccountManager am = AccountManager.get(context);
+
+		String authToken = am.peekAuthToken(account, authTokenType);
+
+		if (!TextUtils.isEmpty(authToken)) {
+			final Bundle result = new Bundle();
+			result.putString(AccountManager.KEY_ACCOUNT_NAME, account.name);
+			result.putString(AccountManager.KEY_ACCOUNT_TYPE, account.type);
+			result.putString(AccountManager.KEY_AUTHTOKEN, authToken);
+			return result;
+		}
+
+		final Intent intent = new Intent(context, MainActivity_.class);
+		intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response);
+		intent.putExtra(Constants.ACCOUNT_TYPE_INTERVENTIX, account.type);
+		intent.putExtra(Constants.ACCOUNT_AUTH_TOKEN, authTokenType);
+		intent.putExtra(Constants.ACCOUNT_NAME, account.name);
+		final Bundle bundle = new Bundle();
+		bundle.putParcelable(AccountManager.KEY_INTENT, intent);
+		return bundle;
+	}
+
+	@Override
+	public String getAuthTokenLabel(String authTokenType) {
+
+		return null;
+	}
+
+	@Override
+	public Bundle hasFeatures(AccountAuthenticatorResponse response, Account account, String[] features) throws NetworkErrorException {
+
+		final Bundle result = new Bundle();
+		result.putBoolean(KEY_BOOLEAN_RESULT, false);
+		return result;
+	}
+
+	@Override
+	public Bundle updateCredentials(AccountAuthenticatorResponse response, Account account, String authTokenType, Bundle options) throws NetworkErrorException {
+
+		return null;
+	}
+
+}
