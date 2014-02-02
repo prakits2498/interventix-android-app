a988c818e5cec4fcdb3a48efa1602dac80e7d8b7
chicco.colantoni <chicco.colantoni@f2c33e9a-98a0-4916-0173-56d017a38c9b>
2014-02-02 13:04:12 +0000

    Versione 0.0.600
    Refactoring dei nomi dei packages delle classi della UI.
    Iniziata l'implementazione della gestione dell'account.

---
+++ Interventix/src/com/federicocolantoni/projects/interventix/authentication/InterventixAuthenticatorService.java
@@ -0,0 +1,15 @@
+package com.federicocolantoni.projects.interventix.authentication;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class InterventixAuthenticatorService extends Service {
+
+	@Override
+	public IBinder onBind(Intent intent) {
+
+		InterventixAuthenticator authenticator = new InterventixAuthenticator(this);
+		return authenticator.getIBinder();
+	}
+}
