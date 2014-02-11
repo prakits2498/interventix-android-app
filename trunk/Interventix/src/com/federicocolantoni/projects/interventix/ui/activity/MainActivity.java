390a17537a413a0ee2e78afeaa0993fd98a36790
chicco.colantoni <chicco.colantoni@f2c33e9a-98a0-4916-0173-56d017a38c9b>
2014-02-11 22:44:08 +0000

    Versione 0.0.750
    Cambiate le icone dell'app... in corso.

--- trunk/Interventix/src/com/federicocolantoni/projects/interventix/ui/activity/MainActivity.java
+++ trunk/Interventix/src/com/federicocolantoni/projects/interventix/ui/activity/MainActivity.java
@@ -20,6 +20,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -30,159 +32,186 @@ import com.federicocolantoni.projects.interventix.Constants;
 import com.federicocolantoni.projects.interventix.R;
 import com.federicocolantoni.projects.interventix.controller.UtenteController;
 import com.federicocolantoni.projects.interventix.entity.Utente;
+import com.federicocolantoni.projects.interventix.settings.SettingActivity;
 import com.federicocolantoni.projects.interventix.utils.ChangeLogDialog;
 import com.j256.ormlite.dao.RuntimeExceptionDao;
 
 @SuppressLint("NewApi")
 @EActivity(R.layout.activity_main)
 public class MainActivity extends ActionBarActivity {
-
-	@ViewById(R.id.tv_changelog)
-	TextView tvChangelog;
-
-	@StringRes(R.string.welcome_title)
-	static String welcomeTitle;
-
-	@StringRes(R.string.welcome_message)
-	static String welcomeMessage;
-
-	private AccountManager accountManager;
-
+    
+    @ViewById(R.id.tv_changelog)
+    TextView tvChangelog;
+    
+    @StringRes(R.string.welcome_title)
+    static String welcomeTitle;
+    
+    @StringRes(R.string.welcome_message)
+    static String welcomeMessage;
+    
+    private AccountManager accountManager;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    
+	super.onCreate(savedInstanceState);
+	
+	accountManager = AccountManager.get(this);
+	
+	BugSenseHandler.initAndStartSession(this, Constants.API_KEY);
+	
+	SharedPreferences prefs = getSharedPreferences(Constants.PREFERENCES, MODE_PRIVATE);
+	
+	String username = prefs.getString(Constants.USERNAME, "");
+	
+	Account[] accounts = accountManager.getAccountsByType(Constants.ACCOUNT_TYPE_INTERVENTIX);
+	
+	boolean utenteTrovato = false;
+	
+	for (Account account : accounts) {
+	    
+	    // passo 3)
+	    if (account.name.equals(username)) {
+		
+		utenteTrovato = true;
+		
+		String pwd = accountManager.getPassword(account);
+		
+		// passo 5)
+		if (pwd == null) {
+		    
+		    FragmentManager manager = getSupportFragmentManager();
+		    
+		    com.federicocolantoni.projects.interventix.modules.login.Login_ fragLogin = new com.federicocolantoni.projects.interventix.modules.login.Login_();
+		    
+		    FragmentTransaction transaction = manager.beginTransaction();
+		    
+		    transaction.replace(R.id.frag_login, fragLogin);
+		    transaction.setCustomAnimations(R.anim.fade_out, R.anim.fade_in);
+		    transaction.commit();
+		    
+		    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+			PreferenceManager.setDefaultValues(getApplicationContext(), R.xml.activity_preferences_options, true);
+		    else
+			PreferenceManager.setDefaultValues(getApplicationContext(), R.xml.activity_support_preferences_options, true);
+		}
+		
+		// passo 4)
+		else {
+		    
+		    RuntimeExceptionDao<Utente, Long> utenteDao = com.federicocolantoni.projects.interventix.Interventix_.getDbHelper().getRuntimeUtenteDao();
+		    
+		    UtenteController.tecnicoLoggato = utenteDao.queryForEq("username", account.name).get(0);
+		    
+		    com.federicocolantoni.projects.interventix.Interventix_.releaseDbHelper();
+		    
+		    startActivity(new Intent(this, com.federicocolantoni.projects.interventix.ui.activity.HomeActivity_.class));
+		}
+		
+		break;
+	    }
+	}
+	
+	// passo 2)
+	if (!utenteTrovato) {
+	    
+	    FragmentManager manager = getSupportFragmentManager();
+	    
+	    com.federicocolantoni.projects.interventix.modules.login.Login_ fragLogin = new com.federicocolantoni.projects.interventix.modules.login.Login_();
+	    
+	    FragmentTransaction transaction = manager.beginTransaction();
+	    
+	    transaction.replace(R.id.frag_login, fragLogin);
+	    transaction.setCustomAnimations(R.anim.fade_out, R.anim.fade_in);
+	    transaction.commit();
+	    
+	    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+		PreferenceManager.setDefaultValues(getApplicationContext(), R.xml.activity_preferences_options, true);
+	    else
+		PreferenceManager.setDefaultValues(getApplicationContext(), R.xml.activity_support_preferences_options, true);
+	}
+    }
+    
+    @Override
+    protected void onStart() {
+    
+	super.onStart();
+	
+	tvChangelog.setOnClickListener(new OnClickListener() {
+	    
+	    @Override
+	    public void onClick(View v) {
+	    
+		// Launch change log dialog
+		ChangeLogDialog changelogDialog = new ChangeLogDialog(MainActivity.this);
+		changelogDialog.show();
+	    }
+	});
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    
+	getMenuInflater().inflate(R.menu.menu_main, menu);
+	
+	return super.onCreateOptionsMenu(menu);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    
+	switch (item.getItemId()) {
+	
+	    case R.id.menu_options:
+		
+		Intent intent = new Intent(this, SettingActivity.class);
+		intent.setAction("com.federicocolantoni.projects.interventix.OPEN_SETTINGS");
+		
+		startActivity(intent);
+		
+		break;
+	}
+	
+	return super.onOptionsItemSelected(item);
+    }
+    
+    public static class FirstRunDialog extends DialogFragment implements OnClickListener {
+	
+	private Button btn_ok;
+	
+	public FirstRunDialog() {
+	
+	}
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-
-		super.onCreate(savedInstanceState);
-
-		accountManager = AccountManager.get(this);
-
-		BugSenseHandler.initAndStartSession(this, Constants.API_KEY);
-
-		SharedPreferences prefs = getSharedPreferences(Constants.PREFERENCES, MODE_PRIVATE);
-
-		String username = prefs.getString(Constants.USERNAME, "");
-
-		Account[] accounts = accountManager.getAccountsByType(Constants.ACCOUNT_TYPE_INTERVENTIX);
-
-		boolean utenteTrovato = false;
-
-		for (Account account : accounts) {
-
-			// passo 3)
-			if (account.name.equals(username)) {
-
-				utenteTrovato = true;
-
-				String pwd = accountManager.getPassword(account);
-
-				// passo 5)
-				if (pwd == null) {
-
-					FragmentManager manager = getSupportFragmentManager();
-
-					com.federicocolantoni.projects.interventix.modules.login.Login_ fragLogin = new com.federicocolantoni.projects.interventix.modules.login.Login_();
-
-					FragmentTransaction transaction = manager.beginTransaction();
-
-					transaction.replace(R.id.frag_login, fragLogin);
-					transaction.setCustomAnimations(R.anim.fade_out, R.anim.fade_in);
-					transaction.commit();
-
-					if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
-						PreferenceManager.setDefaultValues(getApplicationContext(), R.xml.activity_preferences_options, true);
-					else
-						PreferenceManager.setDefaultValues(getApplicationContext(), R.xml.activity_support_preferences_options, true);
-				}
-
-				// passo 4)
-				else {
-
-					RuntimeExceptionDao<Utente, Long> utenteDao = com.federicocolantoni.projects.interventix.Interventix_.getDbHelper().getRuntimeUtenteDao();
-
-					UtenteController.tecnicoLoggato = utenteDao.queryForEq("username", account.name).get(0);
-
-					com.federicocolantoni.projects.interventix.Interventix_.releaseDbHelper();
-
-					startActivity(new Intent(this, com.federicocolantoni.projects.interventix.ui.activity.HomeActivity_.class));
-				}
-
-				break;
-			}
-		}
-
-		// passo 2)
-		if (!utenteTrovato) {
-
-			FragmentManager manager = getSupportFragmentManager();
-
-			com.federicocolantoni.projects.interventix.modules.login.Login_ fragLogin = new com.federicocolantoni.projects.interventix.modules.login.Login_();
-
-			FragmentTransaction transaction = manager.beginTransaction();
-
-			transaction.replace(R.id.frag_login, fragLogin);
-			transaction.setCustomAnimations(R.anim.fade_out, R.anim.fade_in);
-			transaction.commit();
-
-			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
-				PreferenceManager.setDefaultValues(getApplicationContext(), R.xml.activity_preferences_options, true);
-			else
-				PreferenceManager.setDefaultValues(getApplicationContext(), R.xml.activity_support_preferences_options, true);
-		}
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+	
+	    AlertDialog.Builder firstRunDialog = new Builder(getActivity());
+	    
+	    firstRunDialog.setTitle(welcomeTitle);
+	    firstRunDialog.setMessage(welcomeMessage);
+	    firstRunDialog.setIcon(R.drawable.ic_launcher);
+	    
+	    LayoutInflater inflater = getActivity().getLayoutInflater();
+	    View view = inflater.inflate(R.layout.first_run, null);
+	    
+	    firstRunDialog.setView(view);
+	    
+	    btn_ok = (Button) view.findViewById(R.id.save_prefs_url);
+	    btn_ok.setOnClickListener(this);
+	    
+	    return firstRunDialog.create();
 	}
-
+	
 	@Override
-	protected void onStart() {
-
-		super.onStart();
-
-		tvChangelog.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-
-				// Launch change log dialog
-				ChangeLogDialog changelogDialog = new ChangeLogDialog(MainActivity.this);
-				changelogDialog.show();
-			}
-		});
+	public void onClick(View v) {
+	
+	    switch (v.getId()) {
+		case R.id.save_prefs_url:
+		    
+		    dismiss();
+		    break;
+	    }
 	}
-
-	public static class FirstRunDialog extends DialogFragment implements OnClickListener {
-
-		private Button btn_ok;
-
-		public FirstRunDialog() {
-
-		}
-
-		@Override
-		public Dialog onCreateDialog(Bundle savedInstanceState) {
-
-			AlertDialog.Builder firstRunDialog = new Builder(getActivity());
-
-			firstRunDialog.setTitle(welcomeTitle);
-			firstRunDialog.setMessage(welcomeMessage);
-			firstRunDialog.setIcon(R.drawable.ic_launcher);
-
-			LayoutInflater inflater = getActivity().getLayoutInflater();
-			View view = inflater.inflate(R.layout.first_run, null);
-
-			firstRunDialog.setView(view);
-
-			btn_ok = (Button) view.findViewById(R.id.save_prefs_url);
-			btn_ok.setOnClickListener(this);
-
-			return firstRunDialog.create();
-		}
-
-		@Override
-		public void onClick(View v) {
-
-			switch (v.getId()) {
-				case R.id.save_prefs_url:
-
-					dismiss();
-					break;
-			}
-		}
-	}
+    }
 }
--- trunk/Interventix/src/com/federicocolantoni/projects/interventix/ui/activity/ViewInterventoActivity.java
+++ trunk/Interventix/src/com/federicocolantoni/projects/interventix/ui/activity/ViewInterventoActivity.java
@@ -1,6 +1,7 @@
 package com.federicocolantoni.projects.interventix.ui.activity;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import org.androidannotations.annotations.EActivity;
 import org.joda.time.DateTime;
@@ -35,275 +36,285 @@ import com.federicocolantoni.projects.interventix.entity.Cliente;
 import com.federicocolantoni.projects.interventix.entity.DettaglioIntervento;
 import com.federicocolantoni.projects.interventix.entity.Intervento;
 import com.federicocolantoni.projects.interventix.entity.Utente;
-import com.federicocolantoni.projects.interventix.task.GetClientiAsyncTask;
 import com.federicocolantoni.projects.interventix.utils.InterventixToast;
 import com.j256.ormlite.dao.RuntimeExceptionDao;
 
 @SuppressLint("NewApi")
 @EActivity(R.layout.activity_view_intervento)
 public class ViewInterventoActivity extends ActionBarActivity {
-
-	private SharedPreferences prefs;
-
-	private Intervento intervento;
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-
-		super.onCreate(savedInstanceState);
-
-		BugSenseHandler.initAndStartSession(this, Constants.API_KEY);
-
-		getSupportActionBar().setHomeButtonEnabled(true);
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-		new GetClientiAsyncTask().execute();
+    
+    private SharedPreferences prefs;
+    
+    private Intervento intervento;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    
+	super.onCreate(savedInstanceState);
+	
+	BugSenseHandler.initAndStartSession(this, Constants.API_KEY);
+	
+	getSupportActionBar().setHomeButtonEnabled(true);
+	getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+	
+	RuntimeExceptionDao<Cliente, Long> clienteDao = com.federicocolantoni.projects.interventix.Interventix_.getDbHelper().getRuntimeClienteDao();
+	
+	InterventoController.listaClienti = (ArrayList<Cliente>) clienteDao.queryForAll();
+	
+	com.federicocolantoni.projects.interventix.Interventix_.releaseDbHelper();
+    }
+    
+    @Override
+    protected void onStart() {
+    
+	super.onStart();
+	
+	Bundle extras = getIntent().getExtras();
+	
+	prefs = getSharedPreferences(Constants.PREFERENCES, Context.MODE_PRIVATE);
+	
+	getSupportActionBar().setTitle(UtenteController.tecnicoLoggato.nome + " " + UtenteController.tecnicoLoggato.cognome);
+	
+	FragmentManager manager = getSupportFragmentManager();
+	FragmentTransaction transaction = manager.beginTransaction();
+	
+	if (extras != null)
+	    intervento = (Intervento) extras.getSerializable(Constants.INTERVENTO);
+	
+	if (intervento != null) {
+	    
+	    InterventoController.controller = InterventoSingleton.getInstance();
+	    
+	    RuntimeExceptionDao<Intervento, Long> interventoDao = com.federicocolantoni.projects.interventix.Interventix_.getDbHelper().getRuntimeInterventoDao();
+	    
+	    InterventoController.controller.setIntervento(interventoDao.queryForId(intervento.idintervento));
+	    
+	    RuntimeExceptionDao<Cliente, Long> clienteDao = com.federicocolantoni.projects.interventix.Interventix_.getDbHelper().getRuntimeClienteDao();
+	    
+	    InterventoController.controller.setCliente(clienteDao.queryForId(intervento.cliente));
+	    
+	    RuntimeExceptionDao<DettaglioIntervento, Long> dettaglioDao = com.federicocolantoni.projects.interventix.Interventix_.getDbHelper().getRuntimeDettaglioInterventoDao();
+	    
+	    InterventoController.controller.setListaDettagli(dettaglioDao.queryForEq("idintervento", intervento.idintervento));
+	    
+	    RuntimeExceptionDao<Utente, Long> utenteDao = com.federicocolantoni.projects.interventix.Interventix_.getDbHelper().getRuntimeUtenteDao();
+	    
+	    InterventoController.controller.setTecnico(utenteDao.queryForId(InterventoController.controller.getIntervento().tecnico));
+	    
+	    com.federicocolantoni.projects.interventix.Interventix_.releaseDbHelper();
+	    
+	    com.federicocolantoni.projects.interventix.ui.fragments.OverViewInterventoFragment_ overView = new com.federicocolantoni.projects.interventix.ui.fragments.OverViewInterventoFragment_();
+	    
+	    transaction.add(R.id.fragments_layout, overView, Constants.OVERVIEW_INTERVENTO_FRAGMENT);
+	    transaction.addToBackStack(Constants.OVERVIEW_INTERVENTO_FRAGMENT);
+	    transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+	    transaction.commit();
 	}
-
-	@Override
-	protected void onStart() {
-
-		super.onStart();
-
-		Bundle extras = getIntent().getExtras();
-
-		prefs = getSharedPreferences(Constants.PREFERENCES, Context.MODE_PRIVATE);
-
-		getSupportActionBar().setTitle(UtenteController.tecnicoLoggato.nome + " " + UtenteController.tecnicoLoggato.cognome);
-
-		FragmentManager manager = getSupportFragmentManager();
-		FragmentTransaction transaction = manager.beginTransaction();
-
-		if (extras != null)
-			intervento = (Intervento) extras.getSerializable(Constants.INTERVENTO);
-
-		if (intervento != null) {
-
-			InterventoController.controller = InterventoSingleton.getInstance();
-
-			RuntimeExceptionDao<Intervento, Long> interventoDao = com.federicocolantoni.projects.interventix.Interventix_.getDbHelper().getRuntimeInterventoDao();
-
-			InterventoController.controller.setIntervento(interventoDao.queryForId(intervento.idintervento));
-
-			RuntimeExceptionDao<Cliente, Long> clienteDao = com.federicocolantoni.projects.interventix.Interventix_.getDbHelper().getRuntimeClienteDao();
-
-			InterventoController.controller.setCliente(clienteDao.queryForId(intervento.cliente));
-
-			RuntimeExceptionDao<DettaglioIntervento, Long> dettaglioDao = com.federicocolantoni.projects.interventix.Interventix_.getDbHelper().getRuntimeDettaglioInterventoDao();
-
-			InterventoController.controller.setListaDettagli(dettaglioDao.queryForEq("idintervento", intervento.idintervento));
-
-			RuntimeExceptionDao<Utente, Long> utenteDao = com.federicocolantoni.projects.interventix.Interventix_.getDbHelper().getRuntimeUtenteDao();
-
-			InterventoController.controller.setTecnico(utenteDao.queryForId(InterventoController.controller.getIntervento().tecnico));
-
-			com.federicocolantoni.projects.interventix.Interventix_.releaseDbHelper();
-
-			com.federicocolantoni.projects.interventix.ui.fragments.OverViewInterventoFragment_ overView = new com.federicocolantoni.projects.interventix.ui.fragments.OverViewInterventoFragment_();
-
-			transaction.add(R.id.fragments_layout, overView, Constants.OVERVIEW_INTERVENTO_FRAGMENT);
-			transaction.addToBackStack(Constants.OVERVIEW_INTERVENTO_FRAGMENT);
-			transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-			transaction.commit();
-		} else {
-
-			InterventixToast.makeToast("Nuovo intervento", Toast.LENGTH_SHORT);
-
-			InterventoController.controller = InterventoSingleton.getInstance();
-
-			RuntimeExceptionDao<Intervento, Long> interventoDao = com.federicocolantoni.projects.interventix.Interventix_.getDbHelper().getRuntimeInterventoDao();
-
-			long maxNumero = interventoDao.queryRawValue("select max(numero) from Interventi");
-
-			try {
-				Intervento foo = interventoDao.queryBuilder().where().eq("numero", maxNumero).queryForFirst();
-
-				InterventoController.controller.getIntervento().numero = foo.numero + 1;
-			} catch (SQLException e) {
-
-				e.printStackTrace();
-			}
-
-			long maxId = interventoDao.queryRawValue("select max(idintervento) from Interventi");
-
-			try {
-				Intervento foo = interventoDao.queryBuilder().where().eq("idintervento", maxId).queryForFirst();
-
-				InterventoController.controller.getIntervento().idintervento = foo.idintervento + 1;
-			} catch (SQLException e) {
-
-				e.printStackTrace();
-			}
-
-			com.federicocolantoni.projects.interventix.Interventix_.releaseDbHelper();
-
-			InterventoController.controller.getIntervento().dataora = (new DateTime().getMillis());
-			InterventoController.controller.getIntervento().nuovo = (true);
-
-			manager = getSupportFragmentManager();
-			transaction = manager.beginTransaction();
-
-			com.federicocolantoni.projects.interventix.ui.fragments.OverViewInterventoFragment_ overView = new com.federicocolantoni.projects.interventix.ui.fragments.OverViewInterventoFragment_();
-
-			transaction.add(R.id.fragments_layout, overView, Constants.OVERVIEW_INTERVENTO_FRAGMENT);
-			transaction.addToBackStack(Constants.OVERVIEW_INTERVENTO_FRAGMENT);
-			transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-			transaction.commit();
-		}
-
-		final Editor edit = prefs.edit().putInt(Constants.HASH_CODE_INTERVENTO_SINGLETON, InterventoController.controller.hashCode());
-
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD)
-			edit.apply();
-		else {
-
-			new Thread(new Runnable() {
-				@Override
-				public void run() {
-
-					edit.commit();
-				}
-			}).start();
-		}
+	else {
+	    
+	    InterventixToast.makeToast("Nuovo intervento", Toast.LENGTH_SHORT);
+	    
+	    InterventoController.controller = InterventoSingleton.getInstance();
+	    
+	    RuntimeExceptionDao<Intervento, Long> interventoDao = com.federicocolantoni.projects.interventix.Interventix_.getDbHelper().getRuntimeInterventoDao();
+	    
+	    long maxNumero = interventoDao.queryRawValue("select max(numero) from Interventi");
+	    
+	    try {
+		Intervento foo = interventoDao.queryBuilder().where().eq("numero", maxNumero).queryForFirst();
+		
+		InterventoController.controller.getIntervento().numero = foo.numero + 1;
+	    }
+	    catch (SQLException e) {
+		
+		e.printStackTrace();
+	    }
+	    
+	    long maxId = interventoDao.queryRawValue("select max(idintervento) from Interventi");
+	    
+	    try {
+		Intervento foo = interventoDao.queryBuilder().where().eq("idintervento", maxId).queryForFirst();
+		
+		InterventoController.controller.getIntervento().idintervento = foo.idintervento + 1;
+	    }
+	    catch (SQLException e) {
+		
+		e.printStackTrace();
+	    }
+	    
+	    com.federicocolantoni.projects.interventix.Interventix_.releaseDbHelper();
+	    
+	    InterventoController.controller.getIntervento().dataora = (new DateTime().getMillis());
+	    InterventoController.controller.getIntervento().nuovo = (true);
+	    
+	    manager = getSupportFragmentManager();
+	    transaction = manager.beginTransaction();
+	    
+	    com.federicocolantoni.projects.interventix.ui.fragments.OverViewInterventoFragment_ overView = new com.federicocolantoni.projects.interventix.ui.fragments.OverViewInterventoFragment_();
+	    
+	    transaction.add(R.id.fragments_layout, overView, Constants.OVERVIEW_INTERVENTO_FRAGMENT);
+	    transaction.addToBackStack(Constants.OVERVIEW_INTERVENTO_FRAGMENT);
+	    transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+	    transaction.commit();
 	}
-
-	@Override
-	protected void onPause() {
-
-		super.onPause();
-
-		InterventoSingleton.reset();
-	}
-
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-
-		if (keyCode == KeyEvent.KEYCODE_BACK) {
-
-			final FragmentManager manager = getSupportFragmentManager();
-
-			if (manager.getBackStackEntryCount() == 1) {
-
-				checkInterventionsHashcode();
-			} else {
-
-				manager.popBackStackImmediate();
-			}
-			return true;
-		}
-
-		return super.onKeyDown(keyCode, event);
-	}
-
-	@Override
-	public void onBackPressed() {
-
-		final FragmentManager manager = getSupportFragmentManager();
-
-		if (manager.getBackStackEntryCount() == 1) {
-
-			checkInterventionsHashcode();
-		} else {
-			manager.popBackStackImmediate();
-		}
-	}
-
-	public static class ExitIntervento extends DialogFragment implements OnClickListener {
-
-		public ExitIntervento() {
-
-		}
-
+	
+	final Editor edit = prefs.edit().putInt(Constants.HASH_CODE_INTERVENTO_SINGLETON, InterventoController.controller.hashCode());
+	
+	if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD)
+	    edit.apply();
+	else {
+	    
+	    new Thread(new Runnable() {
 		@Override
-		public Dialog onCreateDialog(Bundle savedInstanceState) {
-
-			AlertDialog.Builder exitDialog = new Builder(getActivity());
-
-			exitDialog.setTitle(R.string.interv_mod_title);
-			exitDialog.setMessage(R.string.interv_mod_text);
-
-			exitDialog.setPositiveButton(getString(R.string.yes_btn), this);
-			exitDialog.setNegativeButton(getString(R.string.no_btn), this);
-
-			return exitDialog.create();
+		public void run() {
+		
+		    edit.commit();
 		}
-
-		@Override
-		public void onClick(DialogInterface dialog, int which) {
-
-			switch (which) {
-
-				case DialogInterface.BUTTON_POSITIVE:
-
-					dialog.dismiss();
-
-					saveInterventoOnDB();
-
-					getActivity().finish();
-
-					break;
-
-				case DialogInterface.BUTTON_NEGATIVE:
-
-					dialog.dismiss();
-
-					InterventoSingleton.reset();
-
-					getActivity().finish();
-
-					break;
-			}
-		}
+	    }).start();
 	}
-
+    }
+    
+    @Override
+    protected void onPause() {
+    
+	super.onPause();
+	
+	InterventoSingleton.reset();
+    }
+    
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+    
+	if (keyCode == KeyEvent.KEYCODE_BACK) {
+	    
+	    final FragmentManager manager = getSupportFragmentManager();
+	    
+	    if (manager.getBackStackEntryCount() == 1) {
+		
+		checkInterventionsHashcode();
+	    }
+	    else {
+		
+		manager.popBackStackImmediate();
+	    }
+	    return true;
+	}
+	
+	return super.onKeyDown(keyCode, event);
+    }
+    
+    @Override
+    public void onBackPressed() {
+    
+	final FragmentManager manager = getSupportFragmentManager();
+	
+	if (manager.getBackStackEntryCount() == 1) {
+	    
+	    checkInterventionsHashcode();
+	}
+	else {
+	    manager.popBackStackImmediate();
+	}
+    }
+    
+    public static class ExitIntervento extends DialogFragment implements OnClickListener {
+	
+	public ExitIntervento() {
+	
+	}
+	
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		super.onCreateOptionsMenu(menu);
-
-		return true;
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+	
+	    AlertDialog.Builder exitDialog = new Builder(getActivity());
+	    
+	    exitDialog.setTitle(R.string.interv_mod_title);
+	    exitDialog.setMessage(R.string.interv_mod_text);
+	    
+	    exitDialog.setPositiveButton(getString(R.string.yes_btn), this);
+	    exitDialog.setNegativeButton(getString(R.string.no_btn), this);
+	    
+	    return exitDialog.create();
 	}
-
-	private static void saveInterventoOnDB() {
-
-		if (!InterventoController.controller.getIntervento().nuovo) {
-
-			InterventixToast.makeToast("Intervento " + InterventoController.controller.getIntervento().numero + " aggiornato\n con successo!", Toast.LENGTH_SHORT);
-			InterventoController.updateOnDB();
-		} else {
-
-			InterventixToast.makeToast("Nuovo intervento aggiunto\n con successo!", Toast.LENGTH_SHORT);
-			InterventoController.insertOnDB();
-		}
-	}
-
+	
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-
-		switch (item.getItemId()) {
-
-			case android.R.id.home:
-
-				checkInterventionsHashcode();
-
-				break;
-		}
-
-		return super.onOptionsItemSelected(item);
+	public void onClick(DialogInterface dialog, int which) {
+	
+	    switch (which) {
+	    
+		case DialogInterface.BUTTON_POSITIVE:
+		    
+		    dialog.dismiss();
+		    
+		    saveInterventoOnDB();
+		    
+		    getActivity().finish();
+		    
+		    break;
+		
+		case DialogInterface.BUTTON_NEGATIVE:
+		    
+		    dialog.dismiss();
+		    
+		    InterventoSingleton.reset();
+		    
+		    getActivity().finish();
+		    
+		    break;
+	    }
 	}
-
-	private void checkInterventionsHashcode() {
-
-		int hashCodeIntervento = InterventoController.controller.hashCode();
-
-		if (hashCodeIntervento == prefs.getInt(Constants.HASH_CODE_INTERVENTO_SINGLETON, 0)) {
-
-			InterventoSingleton.reset();
-
-			finish();
-		} else {
-
-			new ExitIntervento().show(getSupportFragmentManager(), Constants.EXIT_INTERVENTO_DIALOG_FRAGMENT);
-		}
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    
+	super.onCreateOptionsMenu(menu);
+	
+	return true;
+    }
+    
+    private static void saveInterventoOnDB() {
+    
+	if (!InterventoController.controller.getIntervento().nuovo) {
+	    
+	    InterventixToast.makeToast("Intervento " + InterventoController.controller.getIntervento().numero + " aggiornato\n con successo!", Toast.LENGTH_SHORT);
+	    InterventoController.updateOnDB();
 	}
+	else {
+	    
+	    InterventixToast.makeToast("Nuovo intervento aggiunto\n con successo!", Toast.LENGTH_SHORT);
+	    InterventoController.insertOnDB();
+	}
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    
+	switch (item.getItemId()) {
+	
+	    case android.R.id.home:
+		
+		checkInterventionsHashcode();
+		
+		break;
+	}
+	
+	return super.onOptionsItemSelected(item);
+    }
+    
+    private void checkInterventionsHashcode() {
+    
+	int hashCodeIntervento = InterventoController.controller.hashCode();
+	
+	if (hashCodeIntervento == prefs.getInt(Constants.HASH_CODE_INTERVENTO_SINGLETON, 0)) {
+	    
+	    InterventoSingleton.reset();
+	    
+	    finish();
+	}
+	else {
+	    
+	    new ExitIntervento().show(getSupportFragmentManager(), Constants.EXIT_INTERVENTO_DIALOG_FRAGMENT);
+	}
+    }
 }
